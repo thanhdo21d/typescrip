e9b0a3feacecea03649dfd4ef218a29eafa3e03c
thanhdo21d <thanhdo9xi@gmail.com>
2023-08-14 21:57:53 +0700

    first commit

---
+++ interface.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role["USERS"] = "USER";
+})(Role || (Role = {}));
+var Size;
+(function (Size) {
+    Size["S"] = "S";
+    Size["M"] = "M";
+    Size["L"] = "L";
+    Size["XL"] = "XL";
+})(Size || (Size = {}));
